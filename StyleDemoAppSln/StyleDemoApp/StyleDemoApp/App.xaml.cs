87c4e2ca1006d5f395e08f7f06ae18f0710fca5d
Allan Pead <adpead@gmail.com>
2022-05-12 06:51:20 +0200

    Initial Commit

---
+++ StyleDemoAppSln/StyleDemoApp/StyleDemoApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StyleDemoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            NavigationPage navPage = new NavigationPage(new MainPage());
+
+            navPage.BarBackgroundColor = Color.Black;
+
+            MainPage = navPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ StyleDemoAppSln/StyleDemoApp/StyleDemoApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StyleDemoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+          //  await Task.Delay(6000);
+
+//            await Navigation.PushAsync(new LoginPage());
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
